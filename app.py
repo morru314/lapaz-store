fb6d7786e6d76fda107f4368d7dd7766d1246fc4
morru314 <a.palahi@gmail.com>
2025-06-14 19:41:24 -0300

    Fix: usar puerto dinámico para Render

--- app.py
+++ app.py
@@ -39,9 +39,10 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-
     with app.app_context():
         from models.models import *
         db.create_all()
 
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
