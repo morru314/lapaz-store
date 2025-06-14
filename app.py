8907944ee45053943575ebe1d9bc3e153e9d04a7
morru314 <a.palahi@gmail.com>
2025-06-14 20:18:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,36 +9,31 @@ from routes.dashboard import dashboard_routes
 from flask import redirect
 import os
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
     db.init_app(app)
-
-    # Registrar Blueprints
     app.register_blueprint(stock_routes)
     app.register_blueprint(clientes_routes)
     app.register_blueprint(ventas_routes)
     app.register_blueprint(deudas_routes)
     app.register_blueprint(dashboard_routes)
 
-    # ✅ Esta será la página principal
     @app.route("/")
     def index():
         return redirect("/dashboard")
 
-    # Filtro personalizado para templates
     @app.template_filter('format_currency')
     def format_currency(value):
         return f"{value:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
     return app
 
+# ✅ Esta línea es la clave para Gunicorn
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     with app.app_context():
         from models.models import *
         db.create_all()
@@ -46,3 +41,4 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
