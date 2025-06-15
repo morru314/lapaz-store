215d5226f58ef08d4f9a3517f404dbd2c63c8e86
morru314 <a.palahi@gmail.com>
2025-06-15 13:07:58 -0300

    Fixed login issues, added password change and sweetalerts

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
 from models.models import User
+from flask_login import current_user  # ya que usás user logueado
+from werkzeug.security import generate_password_hash
+
+
 
 auth_routes = Blueprint('auth_routes', __name__, template_folder='../templates')
 
@@ -21,4 +25,24 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('auth_routes.login'))
+
+
+@auth_routes.route('/perfil', methods=['GET', 'POST'])
+@login_required
+def perfil():
+    if request.method == 'POST':
+        nueva_password = request.form['nueva_password']
+        confirmar_password = request.form['confirmar_password']
+
+        if not nueva_password or not confirmar_password:
+            flash("Todos los campos son obligatorios.")
+        elif nueva_password != confirmar_password:
+            flash("Las contraseñas no coinciden.")
+        else:
+            current_user.set_password(nueva_password)
+            from extensions import db
+            db.session.commit()
+            flash("Contraseña actualizada con éxito.")
+
+    return render_template('perfil.html', user=current_user)
 
